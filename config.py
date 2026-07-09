2b19d07fff74798ddb22ac0e4b98d3ec65784f6c
hasanworks431-ops <hasanworks431@gmail.com>
2026-07-09 17:11:02 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "")
+CHAT_ID = os.getenv("CHAT_ID", "")
+
+GOLD_API_KEY = os.getenv("GOLD_API_KEY", "")
+CURRENCY_API_KEY = os.getenv("CURRENCY_API_KEY", "")
