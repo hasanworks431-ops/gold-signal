14f4bc86a0b5c12e3fefd9bec63b4bc8e2e34b2a
hasanworks431-ops <hasanworks431@gmail.com>
2026-07-11 00:03:32 +0330

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,1 +1,20 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime
+import pytz
 
+
+scheduler = BackgroundScheduler(
+    timezone=pytz.timezone("Asia/Tehran")
+)
+
+
+def start_scheduler(job):
+    scheduler.add_job(
+        job,
+        "cron",
+        day_of_week="sat, sun, mon, tue, wed",
+        hour="11-16",
+        minute="45",
+    )
+
+    scheduler.start()
