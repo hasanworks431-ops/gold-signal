27ca15ad4cef08d24af74d791abe62f641912013
hasanworks431-ops <hasanworks431@gmail.com>
2026-07-11 18:29:34 +0330

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,30 +1,23 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from datetime import datetime
-import pytz
+
+
+scheduler = BackgroundScheduler(timezone="Asia/Tehran")
 
 
 def start_scheduler(job):
 
-    scheduler = BackgroundScheduler(
-        timezone=pytz.timezone("Asia/Tehran")
-    )
+    scheduler.remove_all_jobs()
 
-    # شنبه تا چهارشنبه، هر 30 دقیقه بین 11:45 تا 17:00
     scheduler.add_job(
         job,
-        "cron",
-        day_of_week="sat-wed",
-        hour="11-16",
-        minute="0,30"
+        trigger="cron",
+        day_of_week="sat,sun,mon,tue,wed",
+        hour="11-17",
+        minute="*/15",
+        id="signal_job",
+        replace_existing=True
     )
 
-    # اجرای 17:00
-    scheduler.add_job(
-        job,
-        "cron",
-        day_of_week="sat-wed",
-        hour="17",
-        minute="0"
-    )
-
-    scheduler.start()
+    if not scheduler.running:
+        scheduler.start()
+        
