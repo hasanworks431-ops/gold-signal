5fc561d525d98d32b31a6224e4696f7801bd451d
hasanworks431-ops <hasanworks431@gmail.com>
2026-07-13 19:05:03 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,62 @@
 from flask import Flask
 
+from scheduler import start_scheduler
+from data_api import get_market_data
+from signal_engine import analyze_all_funds
+from telegram_bot import send_message
+
+import asyncio
+
 
 app = Flask(__name__)
+
+
+
+def run_signal():
+
+    print(
+        "RUN_SIGNAL STARTED",
+        flush=True
+    )
+
+    try:
+
+        data = get_market_data()
+
+        message = analyze_all_funds(data)
+
+        asyncio.run(
+            send_message(message)
+        )
+
+        print(
+            "Signal sent successfully",
+            flush=True
+        )
+
+
+    except Exception as e:
+
+        print(
+            f"Signal error: {e}",
+            flush=True
+        )
+
+
+
+# شروع Scheduler هنگام بالا آمدن Flask
+
+start_scheduler(
+    run_signal
+)
+
 
 
 @app.route("/")
 def home():
 
     return "Gold Signal Pro API Running!"
+
 
 
 if __name__ == "__main__":
